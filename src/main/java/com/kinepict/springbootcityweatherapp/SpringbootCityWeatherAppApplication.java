12869e213c6a6993816039504c2019dc8c27fb75
Kattkovacs <kattkovacs@gmail.com>
2021-05-02 21:08:41 +0200

    add CommandLineRunner to SpringbootCityWeatherAppApplication

--- src/main/java/com/kinepict/springbootcityweatherapp/SpringbootCityWeatherAppApplication.java
+++ src/main/java/com/kinepict/springbootcityweatherapp/SpringbootCityWeatherAppApplication.java
@@ -1,19 +1,45 @@
 package com.kinepict.springbootcityweatherapp;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.kinepict.springbootcityweatherapp.service.WeatherAPIService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class SpringbootCityWeatherAppApplication {
-	private static Logger logger = LoggerFactory.getLogger(SpringbootCityWeatherAppApplication.class);
+@Slf4j
+public class SpringbootCityWeatherAppApplication implements CommandLineRunner{
+
+	@Autowired
+	WeatherAPIService weatherAPIService;
+
+	@Value("${city1}")
+	private String city1;
+	@Value("${city2}")
+	private String city2;
+	@Value("${city3}")
+	private String city3;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootCityWeatherAppApplication.class, args);
-			logger.debug("Debug log message");
-			logger.info("Application started");
-			logger.error("Error log message");
+		log.info("Application stopped");
+		log.info("========================");
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("CITY WEATHER APPLICATION");
+		log.info("========================");
+		log.info("Application started");
+		if (args.length == 0){
+			args = new String[]{city1, city2, city3};
+		}
+		for (String arg: args) {
+			log.info("The name of the city is: " + arg);
+			weatherAPIService.getWeatherByCity(arg);
+		}
+	}
 }
